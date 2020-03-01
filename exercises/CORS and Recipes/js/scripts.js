8cc0c3392c83b7e38b69d86ea7c1b5c47baedb77
Sikay <pedrohdezmora@gmail.com>
2020-03-01 17:11:44 +0100

    Añadida funcionalidad al formulario de busqueda

--- exercises/CORS and Recipes/js/scripts.js
+++ exercises/CORS and Recipes/js/scripts.js
@@ -1,10 +1,20 @@
 const baseEndpoint = 'http://www.recipepuppy.com/api/';
 const proxy = 'https://cors-anywhere.herokuapp.com/';
+const form = document.querySelector('form.search');
 
 async function fetchRecipes(query) {
   const response = await fetch(`${proxy}${baseEndpoint}?q=${query}`);
   const recipes = await response.json();
-  console.log(recipes);
+  return recipes;
 }
 
-fetchRecipes('pizza');
+async function handleSubmit(event) {
+  event.preventDefault();
+  const formElement = event.currentTarget;
+  formElement.submit.disabled = true;
+  const recipes = await fetchRecipes(formElement.query.value);
+  console.log(recipes);
+  formElement.submit.disabled = false;
+}
+
+form.addEventListener('submit', handleSubmit);
