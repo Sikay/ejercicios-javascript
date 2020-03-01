bc5915e164b1fa543be0985096b4450d3c31076e
Sikay <pedrohdezmora@gmail.com>
2020-03-01 18:11:34 +0100

    Mostramos todas las recetas

--- exercises/CORS and Recipes/js/scripts.js
+++ exercises/CORS and Recipes/js/scripts.js
@@ -1,6 +1,20 @@
 const baseEndpoint = 'http://www.recipepuppy.com/api/';
 const proxy = 'https://cors-anywhere.herokuapp.com/';
 const form = document.querySelector('form.search');
+const recipesGrid = document.querySelector('.recipes');
+
+function displayRecipes(recipes) {
+  const html = recipes.map(
+    recipe => `<div class="recipe">
+        <h2>${recipe.title}</h2>
+        <p>${recipe.ingredients}</p>
+        ${recipe.thumbnail &&
+          `<img src="${recipe.thumbnail}" alt="${recipe.title}"/>`}
+        <a href="${recipe.href}">View Recipe →</a>
+      </div>`
+  );
+  recipesGrid.innerHTML = html.join('');
+}
 
 async function fetchRecipes(query) {
   const response = await fetch(`${proxy}${baseEndpoint}?q=${query}`);
@@ -13,8 +27,8 @@ async function handleSubmit(event) {
   const formElement = event.currentTarget;
   formElement.submit.disabled = true;
   const recipes = await fetchRecipes(formElement.query.value);
-  console.log(recipes);
   formElement.submit.disabled = false;
+  displayRecipes(recipes.results);
 }
 
 form.addEventListener('submit', handleSubmit);
