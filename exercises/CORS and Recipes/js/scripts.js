e9394f70c78b2bc3d2821fdcd087ff132a565faf
Sikay <pedrohdezmora@gmail.com>
2020-03-01 16:52:35 +0100

    Añadido proxy a la llamada, ahora si se obtienen las recetas

--- exercises/CORS and Recipes/js/scripts.js
+++ exercises/CORS and Recipes/js/scripts.js
@@ -1,7 +1,8 @@
 const baseEndpoint = 'http://www.recipepuppy.com/api/';
+const proxy = 'https://cors-anywhere.herokuapp.com/';
 
 async function fetchRecipes(query) {
-  const response = await fetch(`${baseEndpoint}?q=${query}`);
+  const response = await fetch(`${proxy}${baseEndpoint}?q=${query}`);
   const recipes = await response.json();
   console.log(recipes);
 }
