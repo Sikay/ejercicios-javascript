d9eebbd4e7ffc2b2dc1778eecc330083c1779137
Sikay <pedrohdezmora@gmail.com>
2020-03-01 16:33:36 +0100

    Intentamos obtener las recetas sin exito debido a CORS

---
+++ exercises/CORS and Recipes/js/scripts.js
@@ -0,0 +1,9 @@
+const baseEndpoint = 'http://www.recipepuppy.com/api/';
+
+async function fetchRecipes(query) {
+  const response = await fetch(`${baseEndpoint}?q=${query}`);
+  const recipes = await response.json();
+  console.log(recipes);
+}
+
+fetchRecipes('pizza');
---
+++ exercises/CORS and Recipes/readme.md
@@ -0,0 +1,3 @@
+# CORS and recipes
+
+En este ejercicio vamos a realizar una peticion para obtener una serie de recetas en un servidor donde tienen implementado CORS (Cross-origin resource sharing) para ello utilizaremos parcel.
