cbe548873d8bdf40225ad2bc66fc812d79315a02
Sikay <pedrohdezmora@gmail.com>
2020-03-04 20:46:59 +0100

    Cambiamos add por toggle para que elimine la clase con un segundo click

--- exercises/Flex Panel Gallery/gallery.js
+++ exercises/Flex Panel Gallery/gallery.js
@@ -1,11 +1,13 @@
 const panels = document.querySelectorAll('.panel');
 
 function toggleOpen() {
-  this.classList.add('open'); // Utilizamos this para referirnos a panel
+  this.classList.toggle('open'); // Utilizamos this para referirnos a panel
 }
 
-function toggleActive() {
-  this.classList.add('open-active');
+function toggleActive(e) {
+  if (e.propertyName.includes('flex')) {
+    this.classList.toggle('open-active');
+  }
 }
 
 panels.forEach(panel => panel.addEventListener('click', toggleOpen));
