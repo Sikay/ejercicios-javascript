e39fd1eaa78cad35370e8c0dd9fead80a45ba064
Sikay <pedrohdezmora@gmail.com>
2020-03-04 18:33:16 +0100

    Añadimos animacion al pulsar una tecla

--- exercises/JavaScript Drum Kit/js/drum.js
+++ exercises/JavaScript Drum Kit/js/drum.js
@@ -1,6 +1,16 @@
 window.addEventListener('keydown', function(event) {
   const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
   if (!audio) return;
+  audio.currentTime = 0;
   audio.play();
-  console.log(audio);
+  key.classList.add('playing');
 });
+
+function removeTransition(e) {
+  if (e.propertyName !== 'transform') return;
+  e.originalTarget.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
