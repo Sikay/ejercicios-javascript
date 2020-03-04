6ee5e817dfcbe42486d06589aaf3e3b05c8ec19e
Sikay <pedrohdezmora@gmail.com>
2020-03-04 19:52:35 +0100

    Refactorizacion

--- exercises/JS and CSS Clock/clock.js
+++ exercises/JS and CSS Clock/clock.js
@@ -2,20 +2,26 @@ const secondHand = document.querySelector('.second-hand');
 const minHand = document.querySelector('.min-hand');
 const hourHand = document.querySelector('.hour-hand');
 
-function setDate() {
-  const now = new Date();
-
-  const seconds = now.getSeconds();
+function secondsToDegrees(seconds) {
   const secondsDegrees = (seconds / 60) * 360 + 90;
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+}
 
-  const mins = now.getMinutes();
+function minsToDegrees(mins) {
   const minsDegrees = (mins / 60) * 360 + 90;
   minHand.style.transform = `rotate(${minsDegrees}deg)`;
+}
 
-  const hours = now.getHours();
+function hoursToDegrees(hours) {
   const hoursDegrees = (hours / 12) * 360 + 90;
   hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
 }
 
-setInterval(setDate, 1000);
+function setClockTime() {
+  const now = new Date();
+  secondsToDegrees(now.getSeconds());
+  minsToDegrees(now.getMinutes());
+  hoursToDegrees(now.getHours());
+}
+
+setInterval(setClockTime, 1000);
