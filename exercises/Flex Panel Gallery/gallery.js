4eebbc5796f6904271ee7dd19c2795ae8d8b138c
Sikay <pedrohdezmora@gmail.com>
2020-03-04 20:40:23 +0100

    Al finalizar la transicion de expansion mostramos el texto que ocultamos

--- exercises/Flex Panel Gallery/gallery.js
+++ exercises/Flex Panel Gallery/gallery.js
@@ -4,4 +4,9 @@ function toggleOpen() {
   this.classList.add('open'); // Utilizamos this para referirnos a panel
 }
 
+function toggleActive() {
+  this.classList.add('open-active');
+}
+
 panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
