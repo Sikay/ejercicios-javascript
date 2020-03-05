3bcaebce13847a9ca2089606c686954bdd8af0fc
Sikay <pedrohdezmora@gmail.com>
2020-03-05 19:00:59 +0100

    Añadimos eventos para controlar cuando se mantiene el click del rato y lo mueves

--- exercises/HTML5 Canvas/canvas.js
+++ exercises/HTML5 Canvas/canvas.js
@@ -2,3 +2,18 @@ const canvas = document.querySelector('#draw');
 const context = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+context.strokeStyle = '#BADA55';
+context.lineJoin = 'round';
+context.lineCap = 'round';
+
+let isDrawing = false;
+
+function draw(e) {
+  if (!isDrawing) return;
+  console.log(e);
+}
+
+window.addEventListener('mousemove', draw);
+window.addEventListener('mousedown', () => (isDrawing = true));
+window.addEventListener('mouseup', () => (isDrawing = false));
+window.addEventListener('mouseout', () => (isDrawing = false));
