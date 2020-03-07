7384aef2f7cb88430e4fdb0c42a47ff5d97c1538
Sikay <pedrohdezmora@gmail.com>
2020-03-07 12:07:05 +0100

    Añadida funcionalidad a los ragons de volumen y velocidad del video

--- exercises/Custom Video Player/scripts.js
+++ exercises/Custom Video Player/scripts.js
@@ -23,9 +23,15 @@ function skip() {
   video.currentTime += parseFloat(this.dataset.skip);
 }
 
+function handleRangeUpdate() {
+  video[this.name] = this.value;
+}
+
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
 
 toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(skipButton => skipButton.addEventListener('click', skip));
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
