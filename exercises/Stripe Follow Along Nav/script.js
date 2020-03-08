7485f74071ba6c70b9095e365cbe0443c5d89d22
Sikay <pedrohdezmora@gmail.com>
2020-03-08 13:47:27 +0100

    Añadimos un fondo adaptable para los dropdown

--- exercises/Stripe Follow Along Nav/script.js
+++ exercises/Stripe Follow Along Nav/script.js
@@ -5,11 +5,36 @@ const nav = document.querySelector('.top');
 function handleEnter() {
   this.classList.add('trigger-enter');
   // Utilizamos arrow function para mantener el valor de this
-  setTimeout(() => this.classList.add('trigger-enter-active'), 150);
+  setTimeout(
+    () =>
+      this.classList.contains('trigger-enter') &&
+      this.classList.add('trigger-enter-active'),
+    150
+  );
+  background.classList.add('open');
+
+  const dropdown = this.querySelector('.dropdown');
+  const dropdownCoords = dropdown.getBoundingClientRect(); // Obtenemos datos como tamaño y posicion del elemento
+  const navCoords = nav.getBoundingClientRect();
+
+  const coords = {
+    height: dropdownCoords.height,
+    width: dropdownCoords.width,
+    top: dropdownCoords.top - navCoords.top,
+    left: dropdownCoords.left - navCoords.left,
+  };
+
+  background.style.setProperty('width', `${coords.width}px`);
+  background.style.setProperty('height', `${coords.height}px`);
+  background.style.setProperty(
+    'transform',
+    `translate(${coords.left}px, ${coords.top}px)`
+  );
 }
 
 function handleLeave() {
   this.classList.remove('trigger-enter', 'trigger-enter-active');
+  background.classList.remove('open');
 }
 
 triggers.forEach(trigger =>
