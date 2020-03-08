a1e41a0aca0e5c85f8ec421618f64fe23aa9e225
Sikay <pedrohdezmora@gmail.com>
2020-03-08 17:09:52 +0100

    Creadas funciones para obtener el hoyo y el tiempo de aparicion de forma aleatoria

--- exercises/Whack A Mole/script.js
+++ exercises/Whack A Mole/script.js
@@ -1,3 +1,18 @@
 const holes = document.querySelectorAll('.hole');
 const scoreBoard = document.querySelector('.score');
 const moles = document.querySelectorAll('.mole');
+let lastHole;
+
+function randTime(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+function randomHole(holeElements) {
+  const index = Math.floor(Math.random() * holeElements.length);
+  const hole = holeElements[index];
+  if (hole === lastHole) {
+    return randomHole(holeElements);
+  }
+  lastHole = hole;
+  return hole;
+}
